d0a22deb1395118e027cd1dd8f80f3e4366e0bad
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2019-01-20 14:48:44 +0530

    feat: define database schema

---
+++ database_setup_catalog.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    slug = Column(String(250), nullable=False)
+
+
+class Items(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer, primary_key=True)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    name = Column(String(80), nullable=False)
+    slug = Column(String(250))
+    description = Column(String(250))
+    user_email = Column(String(250))
+    category = relationship(Category)
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
